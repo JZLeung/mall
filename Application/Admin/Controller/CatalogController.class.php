db2011e310fa63d2a574c71dbc72fdb283e219a6
leungjz <leungjz@foxmail.com>
2016-04-08 23:23:25 +0800

    【新增】 用户列表 Signed-off-by: leungjz <leungjz@foxmail.com>

---
+++ Application/Admin/Controller/CatalogController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class CatalogController extends Controller {
+
+	public function index(){
+		
+	}
+
+}
--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -7,32 +7,12 @@ class IndexController extends Controller {
         if (!$admin) {
             $admin = session('?admin') ? session('admin') : null;
             session('admin', $admin);
-            //$this->display('Passport/login-page');
         }else{
-            //$systeminfo = getSystemInfo();
-            //$this->assign('info', $systeminfo);
-            
             session('admin', $admin);
-            //cookie('admin', $admin, 600);
-            //$this->show();
         }
-        //$this->show('_initialize');
         $this->assign('admin', $admin);
     }
     public function index(){
-        //$system = new sysinfo();
-        /*$admin = cookie('admin');
-        if (!$admin) {
-            $admin = session('?admin') ? session('admin') : null;
-            $this->display('Passport/login-page');
-        }else{
-            //$systeminfo = getSystemInfo();
-            //$this->assign('info', $systeminfo);
-            $this->assign('admin', $admin);
-            session('admin', $admin);
-            cookie('admin', $admin, 600);
-            $this->show();
-        }*/
         $this->show();
     }
     public function _empty($name){
@@ -49,13 +29,19 @@ class IndexController extends Controller {
     }
     public function item_list(){
         $items = M('items');
-        //$count = $items->count();// 查询满足要求的总记录数
-        //$Page = new \Think\Page($count,5);// 实例化分页类 传入总记录数和每页显示的记录数(10)
-        //$show = $Page->show();// 分页显示输出
         $data = $items->select();
-        //$data = $items->select();
-       // $this->assign('page',  $show);
         $this->assign('data',  $data);
+        $this->display();
+    }
+
+    public function catalog_list(){
+
+    }
+
+    public function user_list(){
+        $users = M('user');
+        $userList = $users->select();
+        $this->assign('users',  $userList);
         $this->display();
     }
 }
